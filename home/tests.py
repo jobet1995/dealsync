9f8d327418f44338c487b7a9db9837decd04ab34
jobet1995 <jobetcasquejo221@gmail.com>
2025-10-29 17:42:19 +0800

    added test

--- home/tests.py
+++ home/tests.py
@@ -1,11 +1,11 @@
-from django.urls import reverse
+from typing import Any
+from django.test import TestCase
 from home.models import HomePage
 
-from wagtail.models import Page
-from wagtail.test.utils import WagtailPageTestCase
+from wagtail.models import Page, Site
 
 
-class HomeSetUpTests(WagtailPageTestCase):
+class HomeSetUpTests(TestCase):
     """
     Tests for basic page structure setup and HomePage creation.
     """
@@ -21,7 +21,7 @@ class HomeSetUpTests(WagtailPageTestCase):
         self.assertTrue(HomePage.objects.filter(title="Home").exists())
 
 
-class HomeTests(WagtailPageTestCase):
+class HomeTests(TestCase):
     """
     Tests for homepage functionality and rendering.
     """
@@ -30,14 +30,30 @@ class HomeTests(WagtailPageTestCase):
         """
         Create a homepage instance for testing.
         """
+        # Delete any existing homepage
+        HomePage.objects.all().delete()
+        
         root_page = Page.objects.get(pk=1)
-        self.homepage = HomePage(title="Home")
+        self.homepage = HomePage(title="Home", slug="home")
         root_page.add_child(instance=self.homepage)
+        
+        # Delete any existing sites and create a new one with the homepage as the root
+        Site.objects.all().delete()
+        Site.objects.create(
+            hostname='localhost',
+            port=80,
+            root_page=self.homepage,
+            is_default_site=True
+        )
 
-    def test_homepage_status_code(self):
-        response = self.client.get(reverse("home"))
+    def test_homepage_loads(self):
+        response: Any = self.client.get('/')
+        # Check that we get a successful response
         self.assertEqual(response.status_code, 200)
 
-    def test_homepage_template_used(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "home/home_page.html")
+    def test_homepage_contains_welcome_text(self):
+        response: Any = self.client.get('/')
+        # Check that we get a successful response
+        self.assertEqual(response.status_code, 200)
+        # Check that the welcome page content is present
+        self.assertContains(response, "Welcome to your new Wagtail site!")
